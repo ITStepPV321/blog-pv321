00ca15827e9d96a7a255f9f313b0efc376c28ebf
TSShrol <shrolts@gmail.com>
2024-07-19 19:57:39 +0300

    added usersSlice and register to store

--- src/app/appSlices/postsSlice.js
+++ src/app/appSlices/postsSlice.js
@@ -5,12 +5,14 @@ const initialState=[
     {
         id:nanoid(),    //npm i nanoid
         title: "Introduction into Redux/Toolkit",
-        content:"Welcome to the Redux Toolkit Quick Start tutorial! This tutorial will briefly introduce you to Redux Toolkit and teach you how to start using it correctly."
+        content:"Welcome to the Redux Toolkit Quick Start tutorial! This tutorial will briefly introduce you to Redux Toolkit and teach you how to start using it correctly.",
+        userId: 1
     },
     {
         id:nanoid(),    //npm i nanoid
         title: "Create a Redux Store",
-        content:"Create a file named src/app/store.js. Import the configureStore API from Redux Toolkit"
+        content:"Create a file named src/app/store.js. Import the configureStore API from Redux Toolkit",
+        userId: 3
     }
 ];
 
--- src/components/posts/PostsList.jsx
+++ src/components/posts/PostsList.jsx
@@ -6,7 +6,16 @@ const PostsList = () => {
     return (
         <section>
             <h2>Posts</h2>
-            
+            <hr />
+            {
+                posts.map(post => (
+                    <article key={post.id}>
+                        <h3>{post.title}</h3>
+                        <p> {post.content.substring(0, 100)} ...</p>
+                    </article>
+                ))
+            }
+
         </section>
     )
 }
