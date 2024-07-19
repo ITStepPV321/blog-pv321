a85d12f7745487095eac8c4b34e382aa7acd69f7
TSShrol <shrolts@gmail.com>
2024-07-19 21:04:25 +0300

    added posts

--- src/app/appSlices/postsSlice.js
+++ src/app/appSlices/postsSlice.js
@@ -1,28 +1,47 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { nanoid } from "nanoid";
 
-const initialState=[
+const initialState = [
     {
-        id:nanoid(),    //npm i nanoid
+        id: nanoid(),    //npm i nanoid
         title: "Introduction into Redux/Toolkit",
-        content:"Welcome to the Redux Toolkit Quick Start tutorial! This tutorial will briefly introduce you to Redux Toolkit and teach you how to start using it correctly.",
+        content: "Welcome to the Redux Toolkit Quick Start tutorial! This tutorial will briefly introduce you to Redux Toolkit and teach you how to start using it correctly.",
         userId: 1
     },
     {
-        id:nanoid(),    //npm i nanoid
+        id: nanoid(),    //npm i nanoid
         title: "Create a Redux Store",
-        content:"Create a file named src/app/store.js. Import the configureStore API from Redux Toolkit",
+        content: "Create a file named src/app/store.js. Import the configureStore API from Redux Toolkit",
         userId: 3
     }
 ];
 
-export const postsSlice=createSlice({
+export const postsSlice = createSlice({
     name: "posts",
     initialState,
-    reducers:{  // "posts/action"
+    reducers: {  // "posts/action"
+        postAdd: {
+            reducer: (state, action) => {
+                console.log(action.payload);
+                state.push(action.payload);
+            },
+            prepare: (title, content, userId) => {
+                return {
+                    payload: {
+                        id: nanoid(),
+                        title,
+                        content,
+                        userId
+                    }
 
+                }
+            }
+        },
+        postDelete: (state, payload)=>{
+            
+        }
     }
 });
 
-export const {}=postsSlice.actions;
+export const { postAdd } = postsSlice.actions;
 export default postsSlice.reducer;
