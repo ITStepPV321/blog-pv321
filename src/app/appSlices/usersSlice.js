aa15ec6ac72cd560bf59aa76937c209020899229
TSShrol <shrolts@gmail.com>
2024-08-16 19:27:05 +0300

    modefied usersSlice with added AsyncThunk

--- src/app/appSlices/usersSlice.js
+++ src/app/appSlices/usersSlice.js
@@ -1,16 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+const BASE_URL="https://jsonplaceholder.typicode.com/users";
 
-export const initialState=[
-    {id: 1, fullname:"Walter Sckot"},
-    {id: 2, fullname:"Walter White"},
-    {id: 3, fullname:"Winsten Cherchel"},
-];
+// export const initialState=[
+//     {id: 1, fullname:"Walter Sckot"},
+//     {id: 2, fullname:"Walter White"},
+//     {id: 3, fullname:"Winsten Cherchel"},
+// ];
+
+export const fetchUsers=createAsyncThunk('users/fetchUsers',
+async ()=>{
+    const respons=await fetch(BASE_URL);
+    const data=await respons.json();
+    return data;
+}); 
 
 const usersSlice=createSlice({
     name:"users",
-    initialState,
+    initialState:[],
     reducers:{
-
+    },
+    extraReducers(builder){
+        builder.addCase(fetchUsers.fulfilled, (state,action)=>{
+            return action.payload;
+        })
     }
 });
 
